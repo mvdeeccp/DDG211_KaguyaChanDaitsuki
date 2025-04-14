90f4e0b16ba7318adb65a15feaffa91e04ba2b23
mvdeeccp <pongthaveesub@gmail.com>
2025-04-14 18:39:36 +0700

    Game5NOTwork

---
+++ Assets/Scripts/PuzzleTile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PuzzleTile : MonoBehaviour
+{
+    [SerializeField] private Transform[] tiles; 
+    [SerializeField] private TextMeshProUGUI resultText;
+    [SerializeField] private Vector2 gridOrigin = Vector2.zero; 
+    [SerializeField] private float cellSize = 1f; 
+    [SerializeField] private int gridWidth = 4; 
+
+    public void CheckPuzzle()
+    {
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            Vector2 localPos = tiles[i].position - (Vector3)gridOrigin;
+            int col = Mathf.RoundToInt(localPos.x / cellSize);
+            int row = Mathf.RoundToInt(-localPos.y / cellSize);
+
+            int currentIndex = row * gridWidth + col;
+            int expectedIndex = i;
+
+            if (currentIndex != expectedIndex)
+            {
+                resultText.text = "Wrong!";
+                resultText.color = Color.red;
+                return;
+            }
+        }
+
+        resultText.text = "Pass!";
+        resultText.color = Color.green;
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class TileManager : MonoBehaviour
+{
+    [SerializeField] private Transform emptySpace = null;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit)
+            {
+                if (Vector2.Distance(emptySpace.position, hit.transform.position) < 2)
+                {
+                    Vector2 lastEmptySpacePosition = emptySpace.position;
+                    emptySpace.position = hit.transform.position;
+                    hit.transform.position = lastEmptySpacePosition;
+                }
+            }
+        }
+    }
+}
