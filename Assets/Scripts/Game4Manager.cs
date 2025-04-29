7023c40aefd5ea485851c4edb9bafab6ab6bdcb3
mvdeeccp <pongthaveesub@gmail.com>
2025-04-30 03:25:27 +0700

    Game4SettingUpdate

--- Assets/Scripts/Game4Manager.cs
+++ Assets/Scripts/Game4Manager.cs
@@ -1,65 +1,143 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Game4Manager : MonoBehaviour
 {
     public GameObject modelPanel;
     public GameObject gameplayPanel;
-    public List<GameObject> modelBeads; 
-    public int hiddenBeadCount = 6;
     public GameObject resultPanel;
+    public GameObject hintPanel;  
+    public TextMeshProUGUI hintText;
 
-    private HashSet<int> hiddenIndexes = new HashSet<int>(); 
+    public List<GameObject> modelBeads;    
+    public List<GameObject> gameplayBeads;  
+    public int totalRounds = 3;
+
+    private int currentRound = 0;
+    private int[] hiddenBeadCounts = new int[] { 3, 4, 5 };
+    private float[] memoryTimes = new float[] { 3f, 5f, 7f };
+    private HashSet<int> hiddenIndexes = new HashSet<int>();
 
     void Start()
     {
+        resultPanel.SetActive(false);
+        hintPanel.SetActive(false);
         StartCoroutine(ShowModelThenPlay());
     }
 
     IEnumerator ShowModelThenPlay()
     {
-        modelPanel.SetActive(true);            
-        gameplayPanel.SetActive(false);        
+        if (currentRound >= totalRounds)
+        {
+            ShowResult();
+            yield break;
+        }
 
-        
+        modelPanel.SetActive(true);
+        gameplayPanel.SetActive(false);
+
         hiddenIndexes.Clear();
+        ResetModelBeads();
+        ResetGameplayBeads();
 
-        
-        while (hiddenIndexes.Count < hiddenBeadCount)
+        int count = hiddenBeadCounts[currentRound];
+
+        while (hiddenIndexes.Count < count)
         {
             int rand = Random.Range(0, modelBeads.Count);
             hiddenIndexes.Add(rand);
         }
 
-        
         foreach (int index in hiddenIndexes)
         {
-            modelBeads[index].SetActive(false);
+            modelBeads[index].SetActive(false);  
         }
 
-        yield return new WaitForSeconds(3f); 
+        yield return new WaitForSeconds(memoryTimes[currentRound]);
 
-        modelPanel.SetActive(false);         
-        gameplayPanel.SetActive(true);       
+        modelPanel.SetActive(false);
+        gameplayPanel.SetActive(true);
+        ResetGameplayBeads();  
     }
 
-    
+    void ResetModelBeads()
+    {
+        foreach (var bead in modelBeads)
+        {
+            bead.SetActive(true);
+        }
+    }
+
+    void ResetGameplayBeads()
+    {
+        foreach (var bead in gameplayBeads)
+        {
+            bead.SetActive(true);
+        }
+    }
+
     public bool IsCorrect(List<bool> playerRemoved)
     {
-        for (int i = 0; i < modelBeads.Count; i++)
+        bool isCorrect = true;
+
+        for (int i = 0; i < gameplayBeads.Count; i++)
         {
-            bool shouldRemove = hiddenIndexes.Contains(i); 
-            bool playerDidRemove = playerRemoved[i];       
+            bool shouldRemove = hiddenIndexes.Contains(i);
+            bool playerDidRemove = playerRemoved[i];
 
             if (shouldRemove != playerDidRemove)
             {
-                return false; 
+                isCorrect = false;
+                break;
             }
         }
 
-        return true; 
+        currentRound++; 
+
+        if (currentRound >= totalRounds)
+        {
+            ShowResult();
+        }
+        else
+        {
+            StartCoroutine(ShowModelThenPlay());
+        }
+
+        return isCorrect;
+    }
+
+    void ShowResult()
+    {
+        gameplayPanel.SetActive(false);
+        resultPanel.SetActive(true);
+    }
+
+    public void GoToNextLevel()
+    {
+        SceneManager.LoadScene("NextLevelSceneName");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ToggleHint()
+    {
+        if (hintPanel.activeSelf)
+        {
+            hintPanel.SetActive(false);  
+        }
+        else
+        {
+            int totalBeads = modelBeads.Count;
+            int hiddenBeads = hiddenBeadCounts[currentRound];
+
+            hintText.text = $"Level {currentRound + 1} : ? / {hiddenBeads}";
+            hintPanel.SetActive(true);  
+        }
     }
 }
